19f1358f775c5ba6b36af5c99cf18f51c9faf010
nikol-nikto <nikolnikto330@gmail.com>
2026-05-15 02:19:27 +0300

    feat: add experience section

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Experience from "./components/Experience";
 
 function App() {
   const [selectedLanguage, setSelectedLanguage] = useState("Russian");
 
   return (
-    <div className="App min-h-screen justify-center">
+    <div className="min-h-screen justify-center font-nunito bg-[#0a0a0f]">
       <Header
         selectedLanguage={selectedLanguage}
         setSelectedLanguage={setSelectedLanguage}
       />
 
       <Hero selectedLanguage={selectedLanguage} />
+
+      <Experience />
     </div>
   );
 }
